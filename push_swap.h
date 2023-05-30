a02f3737eea8b7a5fdcaa5c58443540db84c41ac
Changhyeon Lee <changhyl@student.42seoul.kr>
2023-05-30 21:22:59 +0900

    1

---
+++ push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: changhyl <changhyl@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 18:53:23 by changhyl          #+#    #+#             */
+/*   Updated: 2023/05/29 12:33:50 by changhyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+#endif
