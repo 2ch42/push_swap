177a92751d999234185a432e10e30eb2cc03b466
ch <ckdgus1120@gmail.com>
2023-07-04 02:46:37 +0900

    1

--- bonus/command_bonus.c
+++ bonus/command_bonus.c
@@ -6,7 +6,7 @@
 /*   By: changhyl <changhyl@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 20:40:57 by changhyl          #+#    #+#             */
-/*   Updated: 2023/07/03 22:41:40 by changhyl         ###   ########.fr       */
+/*   Updated: 2023/07/04 02:45:51 by ch               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,12 @@ static void	one_get_com(t_stack *a, t_stack *b, char *str, int len)
 		swap(b);
 }
 
+static int	err_case(void)
+{
+	write(1, "Error\n", 6);
+	return (-1);
+}
+
 int	get_com(t_stack *a, t_stack *b)
 {
 	char	*str;
@@ -68,16 +74,13 @@ int	get_com(t_stack *a, t_stack *b)
 			|| ft_strncmp(str, "rb\n", len) == 0 || ft_strncmp(str, "rra\n", len)
 			== 0 || ft_strncmp(str, "rrb\n", len) == 0)
 			one_get_com(a, b, str, len);
-		else if (ft_strncmp(str, "ss\n", len) == 0
-			|| ft_strncmp(str, "rr\n", len) == 0
-			|| ft_strncmp(str, "rrr\n", len) == 0)
+		else if (ft_strncmp(str, "ss\n", len) == 0 || ft_strncmp(str, "rr\n", len)
+			== 0 || ft_strncmp(str, "rrr\n", len) == 0)
 			mult_get_com(a, b, str, len);
 		else
-		{
-			write(1, "Error\n", 6);
-			return (-1);
-		}
+			return (err_case());
 		str = get_next_line(0);
 	}
+	free(str);
 	return (1);
 }
